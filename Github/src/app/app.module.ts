45022799b01f28d6a4efa16fff7cee9b1c105c10
ines uwase <uwaseines10@gmail.com>
2019-09-18 17:19:02 +0200

    edited my files

---
+++ Github/src/app/app.module.ts
@@ -0,0 +1,36 @@
+export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UpperCasePipe } from './upper-case.pipe';
+import { GithDirective } from './gith.directive';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    UserFormComponent,
+    UpperCasePipe,
+    GithDirective,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
